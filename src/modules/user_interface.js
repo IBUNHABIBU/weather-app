3cf7e4430fb6f2bcd845eefb46c2257fbdb7b45d
IBUNHABIBU <ibunhabib@gmail.com>
2020-08-13 15:15:41 +0300

    coding the user interface

--- src/modules/user_interface.js
+++ src/modules/user_interface.js
@@ -9,11 +9,17 @@ export function UI(){
   city.innerText = "Dareselaam";
   article.appendChild(city);
   const weather = document.createElement('div');
-  div.className = 'weather';
+  weather.className = 'weather';
   article.appendChild(weather);
   weather.innerHTML= `
   <div class="weather-info">
-      <h3 class="date"> 
+      <h3 class="date"> Thu August 2.55pm </h3>
+      
+      <div class="temp"><h1>28 <span>&#xb0;C</span></h1></div>
+      <div class = "status"></div>
+      <div class="weather-condition">sunny</div>
+      <div class = "hi-lo">29<span>&#xb0;C</span>/21<span>&#xb0;C</span></div>
+      
   </div>
                       `
 }
