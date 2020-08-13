3cf0f8ad3e8d3d3ea88573ad617d750c50dae306
IBUNHABIBU <ibunhabib@gmail.com>
2020-08-13 20:50:09 +0300

    removing lint errors

--- src/modules/app.js
+++ src/modules/app.js
@@ -8,17 +8,16 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5/',
 };
 const search = document.querySelector('.searchbox');
-function getResult(data) {
-  fetch(`${api.base}weather?q=${data}&units=metric&APPID=${api.key}`)
-    .then( response => {
-      return response.json();
-    })
-    .then( displayResult );
-}
-function searchData(e) {
-  if (e.keyCode === 13) {
-    getResult(search.value);
-  }
+
+function getDateFormat(d) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const day = days[d.getDay()];
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+  const date = d.getDate();
+  const time = d.getTime();
+  return `${day} ${date} ${month} ${year} `
 }
 function displayResult(weather) {
   const city = document.querySelector('.city');
@@ -33,16 +32,18 @@ function displayResult(weather) {
   const hiLow = document.querySelector('.hi-lo');
   hiLow.innerHTML = `${weather.main.temp_min}<span>&#xb0;C</span>  /  ${weather.main.temp_max}<span>&#xb0;C</span>`;
 }
+function getResult(data) {
+  fetch(`${api.base}weather?q=${data}&units=metric&APPID=${api.key}`)
+    .then( response => {
+      return response.json();
+    })
+    .then( displayResult );
+}
+function searchData(e) {
+  if (e.keyCode === 13) {
+    getResult(search.value);
+  }
+}
 search.addEventListener('keypress', searchData);
 
-function getDateFormat(d) {
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const days = ['Sunday', 'Monday',  'Tuesday',  'Wednesday',  'Thursday',  'Friday',  'Saturday'];
-  let day = days[d.getDay()];
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-  let date = d.getDate();
-  let time = d.getTime();
-  return `${day} ${date} ${month} ${year} `
-}
 if(module && module.hot) module.hot.accept;
