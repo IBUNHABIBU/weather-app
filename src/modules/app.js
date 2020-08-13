02ea8d040283ca81c0a7fc87885b6b34b2177c31
IBUNHABIBU <ibunhabib@gmail.com>
2020-08-13 19:06:59 +0300

    removing lint errors

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,30 +1,23 @@
 import { UI } from './user_interface';
 
 import '../css/style.css';
+
 UI();
 const api = {
   key: 'd79e7d987356c35ab053eb9c2eb96551',
   base: 'https://api.openweathermap.org/data/2.5/',
 };
 const search = document.querySelector('.searchbox');
-function searchData(e){
+function searchData(e) {
   if (e.keyCode === 13) {
     getResult(search.value);
   }
 }
-search.addEventListener('keypress', searchData);
-function getResult(data) {
-  fetch(`${api.base}weather?q=${data}&units=metric&APPID=${api.key}`)
-  .then( response => {
-    return response.json();
-  })
-  .then( displayResult );
-}
 function displayResult(weather) {
   const city = document.querySelector('.city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
-  let now = new Date();
-  let date = document.querySelector('.date');
+  const now = new Date();
+  const date = document.querySelector('.date');
   date.innerText = getDateFormat(now);
   const temp = document.querySelector('h1');
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>&#xb0;C</span>`;
@@ -33,8 +26,16 @@ function displayResult(weather) {
   const hiLow = document.querySelector('.hi-lo');
   hiLow.innerHTML = `${weather.main.temp_min}<span>&#xb0;C</span>  /  ${weather.main.temp_max}<span>&#xb0;C</span>`;
 }
+search.addEventListener('keypress', searchData);
+function getResult(data) {
+  fetch(`${api.base}weather?q=${data}&units=metric&APPID=${api.key}`)
+    .then( response => {
+      return response.json();
+    })
+    .then( displayResult );
+}
 function getDateFormat(d) {
-  const months =  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const days = ['Sunday', 'Monday',  'Tuesday',  'Wednesday',  'Thursday',  'Friday',  'Saturday'];
   let day = days[d.getDay()];
   let month = months[d.getMonth()];
